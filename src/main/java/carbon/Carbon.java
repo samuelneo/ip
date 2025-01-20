5db7ba99045bd3fea96c9bea43209ff22c494843
samuelneo <24467645+samuelneo@users.noreply.github.com>
2025-01-20 13:54:57 +0800

    Implement todo, event, deadline commands

--- src/main/java/carbon/Carbon.java
+++ src/main/java/carbon/Carbon.java
@@ -1,6 +1,9 @@
 package carbon;
 
+import carbon.task.Deadline;
+import carbon.task.Event;
 import carbon.task.Task;
+import carbon.task.Todo;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -37,6 +40,31 @@ public class Carbon {
         printMessage("Tasks:", taskList);
     }
 
+    private static void printAndAddTask(Task task) {
+        tasks.add(task);
+        boolean pluralise = tasks.size() != 1;
+        printMessage("Added: ",
+                "   " + task.toString(),
+                String.format("You now have %d task%s.", tasks.size(), pluralise ? "s" : ""));
+    }
+
+    private static void addTodo(String arg) {
+        Todo todo = new Todo(arg);
+        printAndAddTask(todo);
+    }
+
+    private static void addDeadline(String arg) {
+        String[] args = arg.split(" /by ");
+        Deadline deadline = new Deadline(args[0], args[1]);
+        printAndAddTask(deadline);
+    }
+
+    private static void addEvent(String arg) {
+        String[] args = arg.split(" /(from|to) ");
+        Event event = new Event(args[0], args[1], args[2]);
+        printAndAddTask(event);
+    }
+
     /**
      * Processes user commands until they enter "bye".
      */
@@ -45,9 +73,10 @@ public class Carbon {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            String input = scanner.nextLine();
-            String[] words = input.toLowerCase().split(" ");
-            String command = words[0];
+            String input = scanner.nextLine().trim();
+            String[] words = input.split(" ", 2);
+            String command = words[0].toLowerCase();
+            String arg = words.length > 1 ? words[1] : "";
 
             switch (command) {
                 case "bye" -> {
@@ -55,19 +84,19 @@ public class Carbon {
                 }
                 case "list" -> printTasks();
                 case "mark" -> {
-                    int index = Integer.parseInt(words[1]) - 1;
+                    int index = Integer.parseInt(arg) - 1;
                     tasks.get(index).markAsDone();
                     printMessage("Marked as done:", tasks.get(index).toString());
                 }
                 case "unmark" -> {
-                    int index = Integer.parseInt(words[1]) - 1;
+                    int index = Integer.parseInt(arg) - 1;
                     tasks.get(index).markAsNotDone();
                     printMessage("Marked as not done:", tasks.get(index).toString());
                 }
-                default -> {
-                    tasks.add(new Task(input));
-                    printMessage("added: " + input);
-                }
+                case "todo" -> addTodo(arg);
+                case "deadline" -> addDeadline(arg);
+                case "event" -> addEvent(arg);
+                default -> printMessage(String.format("Command \"%s\" not recognised.", command));
             }
         }
     }
