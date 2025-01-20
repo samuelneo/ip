001d4e250f61897145f2a0d054c41e74a4dc2ea9
samuelneo <24467645+samuelneo@users.noreply.github.com>
2025-01-20 10:33:53 +0800

    Add indentation to program output

--- src/main/java/Carbon.java
+++ src/main/java/Carbon.java
@@ -1,6 +1,16 @@
 public class Carbon {
+    private static final String horizontalLine = "_".repeat(60);
+
+    /**
+     * Prints the message in between two horizontal lines, with an indentation of 4 spaces
+     */
+    private static void printMessage(String message) {
+        System.out.println("    " + horizontalLine);
+        System.out.println("    " + message.replace("\n", "\n    "));
+        System.out.println("    " + horizontalLine);
+    }
+
     public static void main(String[] args) {
-        String horizontalLine = "_".repeat(60);
         String logo = """
                    ____           _
                   / ___|__ _ _ __| |__   ___  _ __
@@ -9,7 +19,7 @@ public class Carbon {
                   \\____\\__,_|_|  |_.__/ \\___/|_| |_|""";
         String welcomeMessage = "Hello! What can I do for you?";
         String exitMessage = "Goodbye!";
-        System.out.printf("%s\n%s\n\n%s\n", horizontalLine, logo, welcomeMessage);
-        System.out.printf("%s\n%s\n%s\n", horizontalLine, exitMessage, horizontalLine);
+        printMessage(logo + "\n" + welcomeMessage);
+        printMessage(exitMessage);
     }
 }
