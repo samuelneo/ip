69f95ad404810ad8af3c89dbdf3c9e84b538ae10
samuelneo <24467645+samuelneo@users.noreply.github.com>
2025-01-20 10:44:13 +0800

    Implement echo

--- src/main/java/Carbon.java
+++ src/main/java/Carbon.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Carbon {
     private static final String horizontalLine = "_".repeat(60);
 
     /**
-     * Prints the message in between two horizontal lines, with an indentation of 4 spaces
+     * Prints the message in between two horizontal lines, with an indentation of 4 spaces.
      */
     private static void printMessage(String message) {
         System.out.println("    " + horizontalLine);
         System.out.println("    " + message.replace("\n", "\n    "));
-        System.out.println("    " + horizontalLine);
+        System.out.println("    " + horizontalLine + "\n");
+    }
+
+    /**
+     * Processes user commands until they enter "bye".
+     */
+    private static void inputLoop() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                return;
+            }
+            printMessage(input);
+        }
     }
 
     public static void main(String[] args) {
@@ -17,9 +33,8 @@ public class Carbon {
                  | |   / _` | '__| '_ \\ / _ \\| '_ \\
                  | |__| (_| | |  | |_) | (_) | | | |
                   \\____\\__,_|_|  |_.__/ \\___/|_| |_|""";
-        String welcomeMessage = "Hello! What can I do for you?";
-        String exitMessage = "Goodbye!";
-        printMessage(logo + "\n" + welcomeMessage);
-        printMessage(exitMessage);
+        printMessage(logo + "\nHello! What can I do for you?");
+        inputLoop();
+        printMessage("Goodbye!");
     }
 }
