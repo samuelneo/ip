8bf9f2998af16700e113798e20d27f5fe8f55e2e
samuelneo <24467645+samuelneo@users.noreply.github.com>
2025-02-07 21:41:13 +0800

    Refactor miscellaneous sections
    Several code sections fall short of code quality guidelines.
    
    These sections may be less intuitive or readable to others.
    
    Let's update these sections to improve the overall code quality.

--- src/main/java/carbon/utils/Storage.java
+++ src/main/java/carbon/utils/Storage.java
@@ -43,8 +43,6 @@ public class Storage {
      * @return Message describing any abnormalities (empty if none).
      */
     public static String loadDataFile(TaskList taskList) {
-        String message = "";
-
         File dataFile = new File(TASKS_FILE_PATH);
         if (!dataFile.getParentFile().exists() && !dataFile.getParentFile().mkdirs()) {
             throw new RuntimeException("Unable to create data/user/ directory");
@@ -56,46 +54,53 @@ public class Storage {
             throw new RuntimeException(e);
         }
 
-        if (hasFile) {
+        if (!hasFile) {
+            return "";
+        }
+
+        String message = "";
+
+        try {
+            Scanner scanner = new Scanner(dataFile);
+            while (scanner.hasNextLine()) {
+                String firstLine = scanner.nextLine();
+                if (firstLine.isEmpty()) {
+                    break;
+                }
+
+                char type = firstLine.charAt(0);
+                boolean isDone = scanner.nextLine().charAt(0) == '1';
+                String description = scanner.nextLine().trim();
+
+                Task task = switch (type) {
+                    case 'T' -> new Todo(description);
+                    case 'D' -> {
+                        String dueBy = scanner.nextLine().trim();
+                        yield new Deadline(description, dueBy);
+                    }
+                    case 'E' -> {
+                        String from = scanner.nextLine().trim();
+                        String to = scanner.nextLine().trim();
+                        yield new Event(description, from, to);
+                    }
+                    default -> throw new InvalidFileFormatException();
+                };
+
+                if (isDone) {
+                    task.markAsDone();
+                }
+                taskList.add(task);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IndexOutOfBoundsException | NoSuchElementException | InvalidFileFormatException e) {
+            message = "The data file was corrupted. Its contents are ignored and will be reset.\n";
+            taskList.clear();
             try {
-                Scanner scanner = new Scanner(dataFile);
-                while (scanner.hasNextLine()) {
-                    String firstLine = scanner.nextLine();
-                    if (firstLine.isEmpty()) {
-                        break;
-                    }
-                    char type = firstLine.charAt(0);
-                    boolean isDone = scanner.nextLine().charAt(0) == '1';
-                    String description = scanner.nextLine().trim();
-                    Task task = switch (type) {
-                        case 'T' -> new Todo(description);
-                        case 'D' -> {
-                            String dueBy = scanner.nextLine().trim();
-                            yield new Deadline(description, dueBy);
-                        }
-                        case 'E' -> {
-                            String from = scanner.nextLine().trim();
-                            String to = scanner.nextLine().trim();
-                            yield new Event(description, from, to);
-                        }
-                        default -> throw new InvalidFileFormatException();
-                    };
-                    if (isDone) {
-                        task.markAsDone();
-                    }
-                    taskList.add(task);
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IndexOutOfBoundsException | NoSuchElementException | InvalidFileFormatException e) {
-                message = "The data file was corrupted. Its contents are ignored and will be reset.\n";
-                taskList.clear();
-                try {
-                    // Clear tasks.txt contents
-                    new FileWriter(TASKS_FILE_PATH).close();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                // Clear tasks.txt contents
+                new FileWriter(TASKS_FILE_PATH).close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         }
         return message;
